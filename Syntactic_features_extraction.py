375ca6271bdced668d9e20079e5c4d3c3d2b50b9
liatoutou <79835538+liatoutou@users.noreply.github.com>
2023-02-16 17:29:03 +0100

    Update Syntactic_features_extraction.py

--- Syntactic_features_extraction.py
+++ Syntactic_features_extraction.py
@@ -1,35 +1,39 @@
 import spacy
+import benepar
+from nltk import ParentedTree
+
+benepar.download('benepar_en3')
 from spacy import displacy
-import nltk
 
 nlp = spacy.load("en_core_web_sm")
+nlp.add_pipe('benepar', config={'model': 'benepar_en3'})
 print("Please enter your text here:")
 text = input()
 doc = nlp(text)
 
-print("------------------FULL CONSTITUENTS---------------")
-print('Noun phrases:')
+# Extracting all Noun Phrases and head of each NP
+print("------------------NOUN PHRASES---------------\n")
 for chunk in doc.noun_chunks:
-    print('Noun Phrase:', chunk.text, ',', 'NP Head:', chunk.root.text)
+    print('Noun Phrase:', chunk.text, ',', 'NP Head:', chunk.root.text, "\n")
 
-print("------------------TOKENS---------------")
-tagged_text = []
+# Word Tokenization and token's features
+print("------------------TOKENS---------------\n")
 for token in doc:
-    tagged_text.append((token.text, token.pos_))
     print("Token:", token.text, ',', "Lemma:", token.lemma_, ','"POS tag:", token.pos_, ',', "Token Dependency:",
-          token.dep_, ',', "Token Head:", token.head)
-print("-----------------ENTITIES---------------")
-for ent in doc.ents:
-    print('Entity:', ent.text, ',', 'Label:', ent.label_)
+          token.dep_, ',', "Token Head:", token.head, "\n")
 
-grammar = r"""
-  NP: {<DET|ADJ|NOUN.*>+}       
-  PP: {<ADP><NP>}               
-  VP: {<VERB.*><NP|PP|CLAUSE>+$} 
-  CLAUSE: {<NP><VP>}           
-  """
-cp = nltk.RegexpParser(grammar, loop=2)
-result = cp.parse(tagged_text)
-result.draw()
+# Entity Recognition
+print("-----------------ENTITIES---------------\n")
+for ent in doc.ents:
+    print('Entity:', ent.text, ',', 'Label:', ent.label_, "\n")
+
+# Constituency Tree
+sent = list(doc.sents)[0]
+print("-----------------CONSTITUENCY TREE---------------\n")
+print(sent._.parse_string)
+parse_tree = ParentedTree.fromstring('(' + sent._.parse_string + ')')
+print(parse_tree.pretty_print())
+
+# Dependencies Tree
 options = {"collapse_punct": False, "add_lemma": True}
 displacy.serve(doc, style="dep", options=options)
